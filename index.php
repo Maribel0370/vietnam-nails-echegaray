25a57fcaca57fb617c65e2a0ec6a51a866762545
Fernanda Montalvan <fernanda.r.montalvan@gmail.com>
2024-11-28 02:14:20 +0100

    Uploading media, working on main page and css.

--- index.php
+++ index.php
@@ -12,45 +12,79 @@
     <meta name="keywords" content="nail salon, manicure, pedicure">
     <link rel="stylesheet" href="public/Resources/css/style.css">
     <title>Vietnam Nails Echegaray</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     
 </head>
 <body>
     <header class="header">
-        <div class="container">
-            <h1>Vietnam Nails Echegaray</h1>
-            <nav>
-                <ul>
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="public/Webpages/servicios.php">Servicios</a></li>
-                    <li><a href="public/Webpages/reservas.php">Reservas</a></li>
-                    <li><a href="public/Webpages/ofertas.php">Ofertas</a></li>
-                </ul>
-            </nav>
-        </div>
+        <h1>Vietnam Nails Echegaray</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="public/Webpages/servicios.php">Servicios</a></li>
+                <li><a href="public/Webpages/reservas.php">Reservas</a></li>
+                <li><a href="public/Webpages/ofertas.php">Ofertas</a></li>
+            </ul>
+        </nav>
     </header>
 
-    <section id="home" class="hero">
+    <section id="home">
         <div class="container">
             <div class="text-content">
-                <h2>Hundreds of Colors, Organic Nail Polish!</h2>
-                <p>With an obsession for moments, personalized nail enhancement, and lots of tidy colors!</p>
-                <a href="#services" class="btn">Browse Now</a>
+                <h2>Decenas de colores ¡Pintura de uñas Organica!</h2>
+                <p>Con una obsesión por los momentos, servicio personalizado, y muchos colores!</p>
+                <a href="public/Webpages/reservas.php" class="btn">Reserva ahora</a>
             </div>
-            <div class="image-content">
-                <img src="public/Resources/img/" alt="Nail Polish">
-            </div>
+            
+            <img src="public/Resources/img/unas/portrait_nails/nail_face2.jpg" alt="Nail Polish">
         </div>
     </section>
 
-    <section id="about" class="about">
+    <section id="about">
         <div class="container">
-            <h2>About us...</h2>
-            <p>"Every year we help thousands of women feel, and look even more stylish!"</p>
-            <a href="#more-about" class="btn">Read More</a>
+            <div>
+                <h2>Nuestro trabajo...</h2>
+                <p>"Cada año ayudamos a que las mujeres de Barcelona se vean y se sientan mejor!"</p>
+                <a href="#more-about" class="btn">Read More</a>
+            </div>
+
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="public/Resources/img/unas/hand_nails/unas_gel.jpg" class="d-block w-100" alt="uñas de gel">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="public/Resources/img/unas/hand_nails/halloween_nails.jpg" class="d-block w-100" alt="uñas con diseño de Halloween">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="public/Resources/img/unas/hand_nails/green_nails.jpg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+                </div>
         </div>
     </section>
 
-    <section id="services" class="services">
+    <section id="services">
         <div class="container">
             <h2>Take Care of <span>Your Nails</span></h2>
             <p>Paint Them Color Awesome!</p>
@@ -61,7 +95,7 @@
         </div>
     </section>
 
-    <section id="testimonials" class="testimonials">
+    <section id="testimonials">
         <div class="container">
             <h2>Testimonials</h2>
             <div class="testimonial">
@@ -71,33 +105,13 @@
         </div>
     </section>
 
-    <section id="blog" class="blog">
-        <div class="container">
-            <h2>Recent Blog Entries</h2>
-            <div class="blog-entry">
-                <h3>Can Aloe Be Used for Skin Care?</h3>
-                <p>Exploring natural remedies and aloe's benefits for your skin.</p>
-            </div>
-            <div class="blog-entry">
-                <h3>What is Non-Toxic Nail Polish?</h3>
-                <p>Discover safer options for nail care.</p>
-            </div>
-            <div class="blog-entry">
-                <h3>Top Colors for Nail Polish</h3>
-                <p>Stay trendy with the latest nail polish shades.</p>
-            </div>
-        </div>
-    </section>
-
     <footer class="footer">
-        <div class="container">
-            <h2>Get All Special Offers!</h2>
-            <form action="submit.php" method="POST">
-                <input type="email" name="email" placeholder="Enter your email" required>
-                <button type="submit">Subscribe</button>
-            </form>
-            <p>&copy; 2024 Polish Nails. All rights reserved.</p>
-        </div>
+        <h2>Get All Special Offers!</h2>
+        <form action="submit.php" method="POST">
+            <input type="email" name="email" placeholder="Enter your email" required>
+            <button type="submit">Subscribe</button>
+        </form>
+        <p>&copy; 2024 Polish Nails. All rights reserved.</p>
     </footer>
 </body>
 </html>

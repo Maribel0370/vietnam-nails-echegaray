9822873d55301c9d9222051f17a6a97d656efbff
Maribel0370 <chipyblue0370@gmail.com>
2024-12-03 10:43:22 +0100

    logo y favicon
    Se añade logo y favicon

--- setup_files/header.php
+++ setup_files/header.php
@@ -1,3 +1,23 @@
+<?php
+// Iniciar la sesión solo si no está iniciada previamente
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="public/resources/img/icons/favicon_vne.png" type="image/x-icon"> <!-- Añadir favicon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> <!-- Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="public/resources/css/styles.css"> <!-- Archivo de estilos -->
+    <title>Vietnam Nails Echegaray</title> <!-- Updated title -->
+</head>
+<body>
+    
+</body>
+</html>
 <header class="header">
     <h1>Vietnam Nails Echegaray</h1>
 

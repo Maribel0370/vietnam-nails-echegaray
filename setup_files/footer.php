ae36c8da49e4825361064b2ec959c7f21444ed13
Fernanda Montalvan <fernanda.r.montalvan@gmail.com>
2024-12-09 20:32:31 +0100

    Updating footer

--- setup_files/footer.php
+++ setup_files/footer.php
@@ -1,11 +1,23 @@
 
 <footer>
-    <div>
-        <h2><?php echo translate('special_offers', '¡Obtén todas las ofertas especiales!'); ?></h2>
-        <form action="#" method="POST">
-            <input type="email" placeholder="<?php echo translate('enter_email', 'Introduce tu correo'); ?>" class="form-control" name="email" required>
-            <button type="submit" class="btn btn-primary"><?php echo translate('subscribe', 'Suscribirse'); ?></button>
-        </form>
+    <div class="contentFooter">
+        <div>
+            <h2><?php echo translate('special_offers', '¡Obtén todas las ofertas especiales!'); ?></h2>
+            <form action="#" method="POST">
+                <input type="email" placeholder="<?php echo translate('enter_email', 'Introduce tu correo'); ?>" class="form-control" name="email" required>
+                <button type="submit" class="btn btn-primary"><?php echo translate('subscribe', 'Suscribirse'); ?></button>
+            </form>
+            <p>&copy; 2024 Vietnam Nails Echegaray. <?php echo translate('rights_reserved', 'Todos los derechos reservados.'); ?></p>
+        </div>
+        <div>
+            <h2><?php echo translate('<strong>Follow Us</strong>', 'Siguenos');?></h2>
+            <p><?php echo translate('<strong>phone</strong>', '<strong>Teléfono:</strong> 608268978');?></p>
+            <p><?php echo translate('<strong>email</strong>', '<strong>Correo electrónico:</strong>  info@vietnamnails.com');?></p>
+        </div>
+        <div>
+            <h2><?php echo translate('<strong>contact_us</strong>', 'Contáctenos');?></h2>
+            <p><?php echo translate('<strong>phone</strong>', '<strong>Teléfono:</strong> 608268978');?></p>
+            <p><?php echo translate('<strong>email</strong>', '<strong>Correo electrónico:</strong>  info@vietnamnails.com');?></p>
+        </div>
     </div>
-    <p>&copy; 2024 Vietnam Nails Echegaray. <?php echo translate('rights_reserved', 'Todos los derechos reservados.'); ?></p>
 </footer>

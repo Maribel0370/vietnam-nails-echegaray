ae969b55ef44f4828bd5157cf718d85a3a5080ce
Fernanda Montalvan <fernanda.r.montalvan@gmail.com>
2024-12-09 23:33:44 +0100

    Removed form

--- setup_files/footer.php
+++ setup_files/footer.php
@@ -3,25 +3,16 @@
     <div class="contentFooter">
         
         <div class="contacto">
-            <h2><?php echo translate('<strong>contact_us</strong>', 'Contáctenos');?></h2>
-            <p><?php echo translate('<strong>phone</strong>', '<strong>Teléfono:</strong> <a href="https://wa.link/lr6hiq">608268978</a>');?></p>
-            <a href="https://wa.link/lr6hiq"><img src="../public/Resources/img/icons/whatsapp.png" alt="whatsapp link"></a>
-        </div>
-
-        <div>
-            <h2><?php echo translate('special_offers', '¡Obtén todas las ofertas especiales!'); ?></h2>
-            <form action="#" method="POST">
-                <input type="email" placeholder="<?php echo translate('enter_email', 'Introduce tu correo'); ?>" class="form-control" name="email" required>
-                <button type="submit" class="btn btn-primary"><?php echo translate('subscribe', 'Suscribirse'); ?></button>
-            </form>
+            <h2><?php echo translate('<strong>contact_us</strong>', 'Contáctanos');?></h2>
+            <a href="https://wa.link/lr6hiq" target="_blank"><img src="../public/Resources/img/icons/whatsapp.png" alt="whatsapp link"></a>
         </div>
         
 
         <div class="redes">
             <h2><?php echo translate('<strong>Follow Us</strong>', 'Siguenos');?></h2>
             <div>
-                <a href="https://www.facebook.com/share/1LQa8SVbLd/"><img src="../public/Resources/img/icons/facebook.png" alt="Facebook"></a>
-                <a href="https://www.instagram.com/vietnamnailsechegaray?igsh=ZDNoeDU3bGswaDJ5"><img src="../public/Resources/img/icons/instagram.png" alt="Instagram Link"></a>
+                <a href="https://www.facebook.com/share/1LQa8SVbLd/" target="_blank"><img src="../public/Resources/img/icons/facebook.png" alt="Facebook"></a>
+                <a href="https://www.instagram.com/vietnamnailsechegaray?igsh=ZDNoeDU3bGswaDJ5" target="_blank"><img src="../public/Resources/img/icons/instagram.png" alt="Instagram Link"></a>
             </div>
         </div>
     </div>

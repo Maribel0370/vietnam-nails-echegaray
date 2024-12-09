4f18576029114eb5a0816b299ff82c1d1ea23cac
Fernanda Montalvan <fernanda.r.montalvan@gmail.com>
2024-12-09 21:27:20 +0100

    Updating footer and QR code

--- setup_files/footer.php
+++ setup_files/footer.php
@@ -1,25 +1,30 @@
 
 <footer>
     <div class="contentFooter">
+        
+        <div class="contacto">
+            <h2><?php echo translate('<strong>contact_us</strong>', 'Contáctenos');?></h2>
+            <p><?php echo translate('<strong>phone</strong>', '<strong>Teléfono:</strong> <a href="https://wa.link/lr6hiq">608268978</a>');?></p>
+            <a href="https://wa.link/lr6hiq"><img src="../public/Resources/img/icons/whatsapp.png" alt="whatsapp link"></a>
+        </div>
+
         <div>
             <h2><?php echo translate('special_offers', '¡Obtén todas las ofertas especiales!'); ?></h2>
             <form action="#" method="POST">
                 <input type="email" placeholder="<?php echo translate('enter_email', 'Introduce tu correo'); ?>" class="form-control" name="email" required>
                 <button type="submit" class="btn btn-primary"><?php echo translate('subscribe', 'Suscribirse'); ?></button>
             </form>
-            <p>&copy; 2024 Vietnam Nails Echegaray. <?php echo translate('rights_reserved', 'Todos los derechos reservados.'); ?></p>
         </div>
-        <div>
+        
+
+        <div class="redes">
             <h2><?php echo translate('<strong>Follow Us</strong>', 'Siguenos');?></h2>
             <div>
                 <a href="https://www.facebook.com/share/1LQa8SVbLd/"><img src="../public/Resources/img/icons/facebook.png" alt="Facebook"></a>
-                <a href=""><img src="" alt=""></a>
+                <a href="https://www.instagram.com/vietnamnailsechegaray?igsh=ZDNoeDU3bGswaDJ5"><img src="../public/Resources/img/icons/instagram.png" alt="Instagram Link"></a>
             </div>
         </div>
-        <div>
-            <h2><?php echo translate('<strong>contact_us</strong>', 'Contáctenos');?></h2>
-            <p><?php echo translate('<strong>phone</strong>', '<strong>Teléfono:</strong> 608268978');?></p>
-            <p><?php echo translate('<strong>email</strong>', '<strong>Correo electrónico:</strong>  info@vietnamnails.com');?></p>
-        </div>
     </div>
+    
+    <br><p>&copy; 2024 Vietnam Nails Echegaray. <?php echo translate('rights_reserved', 'Todos los derechos reservados.'); ?></p>
 </footer>
